8ca12d2ec9b0b8655e4f1206672514a051853acd
Kamil Cyrkler <contact@kamilc.pro>
2017-10-02 21:55:35 +0200

    Index method

--- src/main/java/com/example/restapi/controllers/UsersController.java
+++ src/main/java/com/example/restapi/controllers/UsersController.java
@@ -1,9 +1,10 @@
 package com.example.restapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,25 +25,30 @@ public class UsersController {
 		this.usersRepository = usersRepository;
 	}
 
+	@GetMapping("users")
+	public List<User> index() {
+		return usersRepository.findAll();
+	}
+
 	@GetMapping("user/{userId}")
 	public User show(@PathVariable Long userId) {
 		return usersRepository.findOne(userId);
 	}
-	
+
 	@PostMapping("users")
 	public User create(@RequestBody User user) {
 		return usersRepository.save(user);
 	}
-	
-	 @PutMapping("/user/{userId}")
-	 public User update (@PathVariable Long userId, @RequestBody User user){
-	  User currentUser = usersRepository.findOne(userId);
-	  currentUser.setEmail(user.getEmail());
-	  currentUser.setFirstName(user.getFirstName());
-	  currentUser.setLastName(user.getLastName());
-	  return usersRepository.save(currentUser);
-	 }
-	 
+
+	@PutMapping("/user/{userId}")
+	public User update(@PathVariable Long userId, @RequestBody User user) {
+		User currentUser = usersRepository.findOne(userId);
+		currentUser.setEmail(user.getEmail());
+		currentUser.setFirstName(user.getFirstName());
+		currentUser.setLastName(user.getLastName());
+		return usersRepository.save(currentUser);
+	}
+
 	@DeleteMapping("user/{userId}")
 	public void delete(@PathVariable Long userId) {
 		usersRepository.delete(userId);
